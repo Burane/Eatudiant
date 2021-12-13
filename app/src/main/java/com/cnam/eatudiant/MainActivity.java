9aabf297963005e9475e6fc51fc6ac41e53d5373
Antoine <kerbiriouAntoine@gmail.com>
2021-12-14 00:01:24 +0100

    splash screen + login

--- app/src/main/java/com/cnam/eatudiant/MainActivity.java
+++ app/src/main/java/com/cnam/eatudiant/MainActivity.java
@@ -1,30 +1,65 @@
 package com.cnam.eatudiant;
 
 import android.content.Intent;
+import android.os.Build;
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowInsets;
 import android.view.WindowInsetsController;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import com.cnam.eatudiant.ui.login.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     private final String TAG = this.getClass().getSimpleName();
+    private final int SPLASH_SCREEN_DELAY = 3000;
+
+    Animation topAnim;
+    Animation bottomAnim;
+    ImageView logo;
+    TextView title;
+    TextView slogan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.i(this.getClass().getSimpleName(),"on create");
 
-    }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            final WindowInsetsController insetsController = getWindow().getInsetsController();
+            if (insetsController != null) {
+                insetsController.hide(WindowInsets.Type.statusBars());
+            }
+        } else {
+            getWindow().setFlags(
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN
+            );
+        }
 
-    public void onGoToLogin(View view) {
-        Log.i(TAG,"clicking onGoToLogin");
-        Intent intent = new Intent(this, LoginActivity.class);
-        startActivity(intent);
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+
+        logo = findViewById(R.id.logo);
+        title = findViewById(R.id.title);
+        slogan = findViewById(R.id.slogan);
+
+        logo.setAnimation(topAnim);
+        title.setAnimation(bottomAnim);
+        slogan.setAnimation(bottomAnim);
+
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+        }, SPLASH_SCREEN_DELAY);
     }
 
 }
