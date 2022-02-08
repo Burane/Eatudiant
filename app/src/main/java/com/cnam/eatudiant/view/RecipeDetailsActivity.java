e4582c18948cf3781e2cd541cfe6036e75e29f83
Antoine <kerbiriouAntoine@gmail.com>
2022-02-08 14:32:52 +0100

    try to get working fragments

--- app/src/main/java/com/cnam/eatudiant/view/RecipeDetailsActivity.java
+++ app/src/main/java/com/cnam/eatudiant/view/RecipeDetailsActivity.java
@@ -1,23 +1,31 @@
 package com.cnam.eatudiant.view;
 
 import android.os.Bundle;
+import android.widget.TableLayout;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.viewpager2.widget.ViewPager2;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import com.cnam.eatudiant.R;
-import com.cnam.eatudiant.fragments.recipe.CookwareFragment;
-import com.cnam.eatudiant.fragments.recipe.IngredientFragment;
-import com.cnam.eatudiant.fragments.recipe.RecipeFragment;
-import com.cnam.eatudiant.fragments.recipe.ViewPager2Adapter;
+import com.cnam.eatudiant.fragments.recipe.*;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 
-public class RecipeDetailsActivity extends AppCompatActivity {
+public class RecipeDetailsActivity extends AppCompatActivity implements TabLayoutMediator.TabConfigurationStrategy{
 
     @BindView(R.id.viewPager2)
     ViewPager2 viewPager2;
+
+    @BindView(R.id.tabLayout)
+    TabLayout tabLayout;
+
+    ArrayList<String> titles;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,20 +33,29 @@ public class RecipeDetailsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_recipe);
         ButterKnife.bind(this);
 
-
+        titles = new ArrayList<>();
+        titles.add("Cookware");
+        titles.add("Recipe steps");
+        titles.add("Ingredients");
+        setViewPagerAdapter();
+        new TabLayoutMediator(tabLayout, viewPager2, this).attach();
     }
 
 
     public void setViewPagerAdapter() {
         ViewPager2Adapter viewPager2Adapter = new ViewPager2Adapter(this);
         ArrayList<Fragment> fragmentList = new ArrayList<>(); //creates
-        fragmentList.add(new CookwareFragment());
-        fragmentList.add(new RecipeFragment());
-        fragmentList.add(new IngredientFragment());
+        fragmentList.add(CookwareFragment.newInstance("",""));
+        fragmentList.add(RecipeStepsFragment.newInstance("",""));
+        fragmentList.add(IngredientFragment.newInstance("",""));
 
         viewPager2Adapter.setFragments(fragmentList);
         viewPager2.setAdapter(viewPager2Adapter);
     }
 
 
+    @Override
+    public void onConfigureTab(@NonNull @NotNull TabLayout.Tab tab, int position) {
+        tab.setText(titles.get(position));
+    }
 }
